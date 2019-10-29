ea797510d9111b91a637e9cbfbdb009c763c531d
William Wyatt <wbw95k@acu.edu>
2019-10-28 21:42:08 -0500

    Refactoring. Added ability to download actual images.

--- generate_html.py
+++ generate_html.py
@@ -7,55 +7,19 @@ import requests
 import shutil
 import sys
 import textwrap
+import traceback
 
 from html.parser import HTMLParser
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
 from requests.adapters import HTTPAdapter
 from requests.exceptions import ConnectionError
+from requests.exceptions import HTTPError
 from time import time, sleep
 
 import numpy as np
 
-LLHEADER = 'https://www.learnedleague.com'
-LLALLCSV = 'https://www.learnedleague.com/lgwide.php?{SEASON_NUM}'
-LOGINFILE = LLHEADER + '/ucp.php?mode=login'
-INPUTDATA = 'logindata.ini'
-
 # Member and Player used interchangeably througout.
-
-# Fetch the playerdata.js file from learnedleague.com.
-# This is list from which the html is built.
-# The piece of code below that pays attention to this
-# switch runs quickly.
-FETCH_PLAYERDATA = True
-
-# As of October 2019 the playerdata.js file does not contain
-# the source for flag images. These URLs can't be determined
-# from the member's name. This switch controls whether the
-# code will
-# 1. Load each member's page.
-# 2. Parse that page looking for the member's flag.
-# This process takes some time since it has to load each member's
-# member page.
-FETCH_FLAGDATA = True
-
-# If the FETCH_FLAGDATA switch is True then this swich controls
-# how many sets of member data will be loaded before the code
-# pauses.
-FETCH_BATCH = 1000
-
-# How many seconds to pause after each batch.
-FETCH_SLEEP = 3
-
-# If LIMIT_FETCH is true then only LIMIT_FETCH_COUNT number of member
-# data will be fetched. This is primarily used for testing instead
-# of loading the X0,000 member records.
-LIMIT_FETCH = False
-LIMIT_FETCH_COUNT = 163
-
-# How many pages of member sets should be output in the HTML menus and pages.
-NUMBER_OF_PAGES = 20
 
 class GetPersonalFlag(HTMLParser):
     """
@@ -109,80 +73,140 @@ class GetPersonalFlag(HTMLParser):
                                     # Set the found person so no more tags are processed.
                                     self.found_person = True
 
-def get_ga_code():
-    """Gets the Google Analytics code if it exists in the logindata.ini file.
-    """
+class FetchAndParseMembers:
+    INPUTDATA = 'logindata.ini'
+
+    # These values may be overwritten from logindata.ini.
+    LLHEADER='https://www.learnedleague.com'
+    LLALLCSV='https://www.learnedleague.com/lgwide.php?{SEASON_NUM}'
+    LOGINFILE=LLHEADER + '/ucp.php?mode=login'
+
+    FETCH_FLAGDATA = True
+    FETCH_BATCH = 1000
+    FETCH_SLEEP = 3
+    LIMIT_FETCH = False
+    LIMIT_FETCH_COUNT = 163
+    NUMBER_OF_PAGES = 20
+    FETCH_IMAGES = False
+
+    def get_ga_code(self):
+        """Gets the Google Analytics code if it exists in the logindata.ini file.
+        """
+        config = configparser.ConfigParser()
+        config.read(self.INPUTDATA)
+        try:
+            google = config['DEFAULT']['google']
+        except KeyError:
+            google = None
+        return google
+
+    def get_session(self):
+        """
+        Read an ini file, establish a login session
+
+        Input:
+            inifile -- name of local ini file with control information
+
+        Returns: logged in requests session to be used in later operations.
+        This code is based on llama_slobber session get_function().
+        """
+        config = configparser.ConfigParser()
+        config.read(self.INPUTDATA)
+        payload = {'login': 'Login'}
+        for attrib in ['username', 'password']:
+            payload[attrib] = config['DEFAULT'][attrib]
+        ses1 = requests.Session()
+        try:
+            loginfile = config['DEFAULT']['loginfile']
+        except KeyError:
+            loginfile = self.LOGINFILE
+        ses1.post(loginfile, data=payload)
+        return ses1
+
+    def get_page_data(self, url=None, parser=None, session=None):
+        """
+        Gets and parse an HTML page.
+        """
+        if session is None:
+            session = self.get_session()
+        main_data = session.get(url)
+        # Good response?
+        if main_data.status_code == 200:
+            parser1 = parser
+            parser1.feed(main_data.text)
+            return parser1.result
+        else:
+            r = main_data.response
+            print(f'Problem parsing {url}. {r}')
+            return None
+
+    # def log_result(self, retval):
+    #     results.append(retval)
+    #     print('{}'.format(retval))
+
+    def fetch_player(self, session, player=None, header=LLHEADER):
+        '''
+        Fetch a player's member page and the URL for the XL flag.
+        '''
+        if session is None:
+            session = self.get_session()
+        link = player['player_link']
+        page = f'{header}{link}'
+        flag = self.get_page_data(page, GetPersonalFlag(), session)
+        if 'flag_src' in flag:
+            player['player_flag'] = flag['flag_src']
+
+        return player
+
+    def fetch_flag(self, member, header):
+        flag_extension = member['player_flag'][-3:]
+        if flag_extension == 'gif':
+            image_src = member['player_flag']
+            local_folder_path = Path(member['player_flag'][1:]).parent
+            local_file_path = Path(member['player_flag'][1:])
+
+            # Create directories if the they don't exist
+            local_folder_path.mkdir(parents=True, exist_ok=True)
+
+            try:
+                img_file = requests.get(header + image_src)
+                img_file.raise_for_status()
+                with open(local_file_path, 'wb') as f:
+                    f.write(img_file.content)
+            except HTTPError as http_err:
+                return {'status': 'fail', 'member': member, 'error': http_err}
+            except Exception as e:
+                return {'status': 'fail', 'tried': header + image_src, 'exception': traceback.format_exc()}
+        return {'status': 'success', 'member': member }
+
+    def __init__(self):
+        pass
+
+def main():
+    ll = FetchAndParseMembers()
+
     config = configparser.ConfigParser()
-    config.read(INPUTDATA)
-    try:
-        google = config['DEFAULT']['google']
-    except KeyError:
-        google = None
-    return google
+    config.read(ll.INPUTDATA)
 
+    ll.LLHEADER = config['DEFAULT']['LLHEADER']
+    ll.LOGINFILE = ll.LLHEADER + '/ucp.php?mode=login'
 
-def get_session():
-    """
-    Read an ini file, establish a login session
+    ll.FETCH_PLAYERDATA = config['DEFAULT'].getboolean('FETCH_PLAYERDATA')
+    ll.FETCH_FLAGDATA = config['DEFAULT'].getboolean('FETCH_FLAGDATA')
+    ll.FETCH_BATCH = config['DEFAULT'].getint('FETCH_BATCH')
+    ll.FETCH_SLEEP = config['DEFAULT'].getint('FETCH_SLEEP')
+    ll.LIMIT_FETCH = config['DEFAULT'].getboolean('LIMIT_FETCH')
+    ll.LIMIT_FETCH_COUNT = config['DEFAULT'].getint('LIMIT_FETCH_COUNT')
+    ll.NUMBER_OF_PAGES = config['DEFAULT'].getint('NUMBER_OF_PAGES')
+    ll.FETCH_IMAGES = config['DEFAULT'].getboolean('FETCH_IMAGES')
 
-    Input:
-        inifile -- name of local ini file with control information
-
-    Returns: logged in requests session to be used in later operations.
-    This code is based on llama_slobber session get_function().
-    """
-    config = configparser.ConfigParser()
-    config.read(INPUTDATA)
-    payload = {'login': 'Login'}
-    for attrib in ['username', 'password']:
-        payload[attrib] = config['DEFAULT'][attrib]
-    ses1 = requests.Session()
-    try:
-        loginfile = config['DEFAULT']['loginfile']
-    except KeyError:
-        loginfile = LOGINFILE
-    ses1.post(loginfile, data=payload)
-    return ses1
-
-def get_page_data(url, parser, session=get_session()):
-    """
-    Gets and parse an HTML page.
-    """
-    main_data = session.get(url)
-    # Good response?
-    if main_data.status_code == 200:
-        parser1 = parser
-        parser1.feed(main_data.text)
-        return parser1.result
-    else:
-        r = main_data.response
-        print(f'Problem parsing {url}. {r}')
-        return None
-
-def log_result(retval):
-    results.append(retval)
-    print('{}'.format(retval))
-
-def fetch_player(session=get_session(), player=None, header=LLHEADER):
-    '''
-    Fetch a player's member page and the URL for the XL flag.
-    '''
-    link = player['player_link']
-    page = f'{header}{link}'
-    flag = get_page_data(page, GetPersonalFlag(), session=session)
-    if 'flag_src' in flag:
-        player['player_flag'] = flag['flag_src']
-
-    return player
-
-if __name__ == "__main__":
-    session = get_session()
+    session = ll.get_session()
 
     # Do we need to fetch the playerdata.js file from learnedleague.com?
-    if FETCH_PLAYERDATA == True:
+    if ll.FETCH_PLAYERDATA == True:
         # Get and parse scripts/playerdata.js
         try:
-            playerdata = session.get(f'{LLHEADER}/scripts/playerdata.js')
+            playerdata = session.get(f'{ll.LLHEADER}/scripts/playerdata.js')
         except ConnectionError as ce:
             print(ce)
         if playerdata.status_code == 200:
@@ -282,13 +306,13 @@ if __name__ == "__main__":
 
     # If the LIMIT_FETCH switch is set then we only need the first LIMIT_FETCH_COUNT
     # players.
-    if LIMIT_FETCH:
-        player_list = player_list[0:LIMIT_FETCH_COUNT]
+    if ll.LIMIT_FETCH:
+        player_list = player_list[0:ll.LIMIT_FETCH_COUNT]
 
     # As of the end of LL82 the playerdata.js file does not contain the URL of the member's
     # flag (although there is a placeholder for it.)
     # Do we need to fetch the flag URL for each member?
-    if FETCH_FLAGDATA == True:
+    if ll.FETCH_FLAGDATA == True:
         # results will hold the URL for each member where a flag is found.
         results = []
 
@@ -305,7 +329,7 @@ if __name__ == "__main__":
         batch_count = 1
 
         # How many members in each batch?
-        total_batches = math.floor((len(player_list) / FETCH_BATCH))
+        total_batches = math.floor((len(player_list) / ll.FETCH_BATCH))
 
         # Run through all the players loaded from the pickled user object.
         while i < len(player_list):
@@ -313,7 +337,7 @@ if __name__ == "__main__":
             small_list.append(player_list[i])
 
             # If the progress into the small_list == the number to fetch in a batch
-            if len(small_list) % FETCH_BATCH == 0:
+            if len(small_list) % ll.FETCH_BATCH == 0:
 
                 # Set up a python multiprocessing threadpool.
                 pool = ThreadPool(5)
@@ -322,7 +346,7 @@ if __name__ == "__main__":
                 # pass in the player object to fetch and parse the member page.
                 # The member flag URL, if found, will be populated in the player object.
                 for player in small_list:
-                    small_results.append(pool.apply_async(fetch_player, args=(session, player, LLHEADER )))
+                    small_results.append(pool.apply_async(ll.fetch_player, args=(session, player, ll.LLHEADER )))
 
                 pool.close()
                 pool.join()
@@ -333,9 +357,9 @@ if __name__ == "__main__":
 
                 # How long did this batch take?
                 this_batch = (time() - ts)
-                print(f'Batch: {batch_count} of {total_batches}. Fetching {FETCH_BATCH} image URLs in: {this_batch}')
+                print(f'Batch: {batch_count} of {total_batches}. Fetching {ll.FETCH_BATCH} image URLs in: {this_batch}')
                 # Try to play nice...
-                sleep(FETCH_SLEEP)
+                sleep(ll.FETCH_SLEEP)
 
                 # Ready for next batch.
                 ts = time()
@@ -348,14 +372,14 @@ if __name__ == "__main__":
         # TODO DRY this code.
         pool = ThreadPool(5)
         for player in small_list:
-            small_results.append(pool.apply_async(fetch_player, args=(session, player, LLHEADER )))
+            small_results.append(pool.apply_async(ll.fetch_player, args=(session, player, ll.LLHEADER )))
         pool.close()
         pool.join()
         for r in small_results:
             results.append(r.get())
 
         this_batch = (time() - ts)
-        print(f'Batch: {batch_count} of {total_batches}. Fetching {FETCH_BATCH} image URLs in: {this_batch}')
+        print(f'Batch: {batch_count} of {total_batches}. Fetching {ll.FETCH_BATCH} image URLs in: {this_batch}')
 
         print('fetched{}'.format(len(results)))
 
@@ -365,7 +389,7 @@ if __name__ == "__main__":
 
     # load member data
     if Path('pickles', 'members.pkl').is_file():
-        with open(f'pickles/members.pkl', 'rb') as f:
+        with open(Path('pickles', 'members.pkl'), 'rb') as f:
             member_data = pickle.load(f)
     else:
         print('pickes/members.pkl not found. Change FETCH_FLAGDATA to True.')
@@ -374,16 +398,16 @@ if __name__ == "__main__":
     print('Generating js/players.js file.')
     player_js_output = 'var members = [];\n'
     page_counter = 1
-    members_per_page = math.floor(len(member_data)/NUMBER_OF_PAGES)
+    members_per_page = math.floor(len(member_data)/ll.NUMBER_OF_PAGES)
     i = 0
     member_counter = 1
     for member in member_data:
         memberName = member['player_name']
-        memberProfile = LLHEADER + member['player_link']
+        memberProfile = ll.LLHEADER + member['player_link']
         if member['player_flag'] == None:
             flagUrl = ''
         else:
-            flagUrl = LLHEADER + member['player_flag']
+            flagUrl = ll.LLHEADER + member['player_flag']
         if i > members_per_page:
             page_counter += 1
             i = 0
@@ -398,7 +422,7 @@ if __name__ == "__main__":
 
     # Write the Google analytics tracking file.
     print('Generating js/ga.js file.')
-    ga_code = get_ga_code()
+    ga_code = ll.get_ga_code()
     if ga_code:
         with open(Path('ga.html'), 'r', encoding="utf-8") as f:
             g = f.read()
@@ -408,4 +432,78 @@ if __name__ == "__main__":
     with open(Path('ll', 'js', 'ga.js'), 'w', encoding="utf-8") as f:
         f.write(g)
 
+    if ll.FETCH_IMAGES:
+        # member_data still has the list of members
+        # Get each member's imagee. These are available when not logged in.
+        # Fetch the image without logging in. Hopefully this lessens the load on
+        # the LL server.
+
+        # results will hold the URL for each member where a flag is found.
+        results = []
+
+        # small_results will hold the results for an individual batch.
+        small_results = []
+
+        # When did the first batch start?
+        ts = time()
+
+        i = 0
+        small_list = []
+
+        # for logging
+        batch_count = 1
+
+        # How many members in each batch?
+        total_batches = math.floor((len(player_list) / ll.FETCH_BATCH))
+
+        # Run through all the players loaded from the pickled member_data object.
+        while i < len(member_data):
+            # add this player to the small_list.
+            small_list.append(member_data[i])
+
+            # If the progress into the small_list == the number to fetch in a batch
+            if len(small_list) % ll.FETCH_BATCH == 0:
+
+                # Set up a python multiprocessing threadpool.
+                pool = ThreadPool(5)
+
+                # For each player in the small batch
+                # pass in the player object to fetch and parse the member page.
+                # The member flag URL, if found, will be populated in the player object.
+                for member in small_list:
+                    small_results.append(pool.apply_async(ll.fetch_flag, args=(member, ll.LLHEADER )))
+
+                pool.close()
+                pool.join()
+
+                # for all players in the small_results append that player into the larger list.
+                for r in small_results:
+                    results.append(r.get())
+
+                # How long did this batch take?
+                this_batch = (time() - ts)
+                print(f'Batch: {batch_count} of {total_batches}. Fetching {ll.FETCH_BATCH} image URLs in: {this_batch}')
+                # Try to play nice...
+                sleep(ll.FETCH_SLEEP)
+
+                # Ready for next batch.
+                ts = time()
+                batch_count += 1
+                small_list = []
+                small_results = []
+            i += 1
+
+        # Fetch and parse any left over after last small_list.
+        # TODO DRY this code.
+        pool = ThreadPool(5)
+        for member in small_list:
+            small_results.append(pool.apply_async(ll.fetch_flag, args=(member, ll.LLHEADER )))
+        pool.close()
+        pool.join()
+        for r in small_results:
+            results.append(r.get())
+
     print('Done.')
+
+if __name__ == "__main__":
+    main()
